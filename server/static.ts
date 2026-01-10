bee3566faa79faf27d4a9a8953c13200e9e41485
yaeugen12 <yaeugen12@gmail.com>
2026-01-10 00:43:43 +0000

    Fix: __dirname undefined in ESM (static.ts)

--- server/static.ts
+++ server/static.ts
@@ -1,9 +1,14 @@
 import express, { type Express } from "express";
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
+
+// ESM equivalent of __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export function serveStatic(app: Express) {
-  const distPath = path.resolve(__dirname, "public");
+  const distPath = path.resolve(__dirname, "../dist/public");
   if (!fs.existsSync(distPath)) {
     throw new Error(
       `Could not find the build directory: ${distPath}, make sure to build the client first`,
